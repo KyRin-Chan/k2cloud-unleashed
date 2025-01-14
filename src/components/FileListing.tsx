1dbd4d83ee749323b901de9d797fff4e8769fa11
K2Lin-Daniel <77517478+K2Lin-Daniel@users.noreply.github.com>
2025-01-14 20:05:50 +0800

    no plyr

--- src/components/FileListing.tsx
+++ src/components/FileListing.tsx
@@ -390,7 +390,7 @@ const FileListing: FC<{ query?: ParsedUrlQuery }> = ({ query }) => {
 
   if ('file' in responses[0] && responses.length === 1) {
     const file = responses[0].file as OdFileObject
-    const previewType = getPreviewType(getExtension(file.name), { video: Boolean(file.video) })
+    const previewType = getPreviewType(getExtension(file.name), { video: false })
 
     if (previewType) {
       switch (previewType) {
